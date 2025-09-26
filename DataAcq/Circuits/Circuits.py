51deee06618e9de0373fc70deac3623845b70937
wyguy27 <wms63@case.edu>
2025-09-26 16:42:50 -0400

    change circuits

--- DataAcq/Circuits/Circuits.py
+++ DataAcq/Circuits/Circuits.py
@@ -1,48 +1,50 @@
 import requests
 import json
 
+baseUrl = "https://api.jolpi.ca/ergast/f1/"
+
 def getCircuitsByYear(year: int):
-    url = f"https://api.jolpi.ca/ergast/f1/{year}/circuit.json"
+    url = baseUrl + f"{year}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 def getCircuitByRound(year: int, round: int):
-    url = f"https://api.jolpi.ca/ergast/f1/{year}/{round}/circuit.json"
+    url = baseUrl + f"{year}/{round}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 def getCircuitByID(year: int, circuitid: str):
-    url = f"https://api.jolpi.ca/ergast/f1/{year}/circuits/{circuitid}/circuits.json"
+    url = baseUrl + f"{year}/circuits/{circuitid}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 # returns circuits that a specific constructor has participated in
 def getCircuitsByConstructor(constructorid: str):
-    url = f"https://api.jolpi.ca/ergast/f1/constructors/{constructorid}/circuits.json"
+    url = baseUrl + f"constructors/{constructorid}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 # returns circuits that a specific driver has participated in
 def getCircuitsByDriver(driverid: str):
-    url = f"https://api.jolpi.ca/ergast/f1/drivers/{driverid}/circuits.json"
+    url = baseUrl + f"drivers/{driverid}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 # returns circuits that a specific driver has participated in
 def getCircuitByDriver(driverid: str):
-    url = f"https://api.jolpi.ca/ergast/f1/drivers/{driverid}/circuits.json"
+    url = baseUrl + f"drivers/{driverid}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
 
 # returns circuits that a specific driver has participated in
 def getCircuitsByStatus(statusid: str):
-    url = f"https://api.jolpi.ca/ergast/f1/status/{statusid}/circuits.json"
+    url = baseUrl + f"status/{statusid}/circuits"
     response = requests.get(url)
     data = response.json()
     return data
